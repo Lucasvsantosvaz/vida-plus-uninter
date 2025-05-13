cb11731fe6aee68a2fba5b0fe4ce0e3a6bb60073
Lucas Vieira <lucas.v.santosvaz@hotmail.com>
2025-05-12 22:32:23 -0300

    primeiro commit

---
+++ force-app/main/default/lwc/cadastroPaciente/cadastroPaciente.js
@@ -0,0 +1,129 @@
+import { LightningElement, track } from 'lwc';
+import { createRecord } from 'lightning/uiRecordApi';
+import { NavigationMixin } from 'lightning/navigation';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+// Importa o objeto
+import PACIENTE_OBJECT from '@salesforce/schema/Paciente__c';
+
+// Importa os campos
+import NAME_FIELD from '@salesforce/schema/Paciente__c.Name';
+import TELEFONE_FIXO_FIELD from '@salesforce/schema/Paciente__c.Telefone__c';
+import CELULAR_FIELD from '@salesforce/schema/Paciente__c.Celular__c';
+import EMAIL_FIELD from '@salesforce/schema/Paciente__c.Email__c';
+import CEP_FIELD from '@salesforce/schema/Paciente__c.CEP__c';
+import ESTADO_FIELD from '@salesforce/schema/Paciente__c.Estado__c';
+import CIDADE_FIELD from '@salesforce/schema/Paciente__c.Cidade__c';
+import BAIRRO_FIELD from '@salesforce/schema/Paciente__c.Bairro__c';
+import RUA_FIELD from '@salesforce/schema/Paciente__c.Rua__c';
+import NUMERO_FIELD from '@salesforce/schema/Paciente__c.Numero__c';
+import COMPLEMENTO_FIELD from '@salesforce/schema/Paciente__c.Complemento__c';
+
+// Importa o método getEndereco do Apex
+import getEndereco from '@salesforce/apex/CadastroPacienteController.getAddressByCEP';
+
+export default class CadastroPaciente extends NavigationMixin(LightningElement) {
+    showSpinner = false;
+    cadastrarDisable = true;
+
+    @track name = '';
+    @track telefoneFixo = '';
+    @track telefoneCelular = '';
+    @track email = '';
+    @track cep = '';
+    @track estado = '';
+    @track cidade = '';
+    @track bairro = '';
+    @track rua = '';
+    @track numero = '';
+    @track complemento = '';
+
+    handleInputChange(event) {
+        const field = event.target.name;
+        this[field] = event.target.value;
+        this.validateRequiredFields();
+    }
+
+    validateRequiredFields() {
+        const camposObrigatoriosPreenchidos =
+            this.name?.trim() &&
+            this.telefoneCelular?.trim() &&
+            this.estado?.trim() &&
+            this.cidade?.trim() &&
+            this.bairro?.trim() &&
+            this.rua?.trim() &&
+            this.numero?.trim();
+
+        this.cadastrarDisable = !camposObrigatoriosPreenchidos;
+    }
+
+    async handlePesquisarCEP() {
+        this.showSpinner = true;
+        try {
+            const result = await getEndereco({ cep: this.cep });
+            this.estado = result.uf;
+            this.cidade = result.localidade;
+            this.bairro = result.bairro;
+            this.rua = result.logradouro;
+            this.validateRequiredFields();
+        } catch (error) {
+            console.error(error);
+            alert('Erro ao buscar o endereço.');
+        } finally {
+            this.showSpinner = false;
+        }
+    }
+
+    async handleSubmit() {
+        this.showSpinner = true;
+        const fields = {};
+        fields[NAME_FIELD.fieldApiName] = this.name;
+        fields[TELEFONE_FIXO_FIELD.fieldApiName] = this.telefoneFixo;
+        fields[CELULAR_FIELD.fieldApiName] = this.telefoneCelular;
+        fields[EMAIL_FIELD.fieldApiName] = this.email;
+        fields[CEP_FIELD.fieldApiName] = this.cep;
+        fields[ESTADO_FIELD.fieldApiName] = this.estado;
+        fields[CIDADE_FIELD.fieldApiName] = this.cidade;
+        fields[BAIRRO_FIELD.fieldApiName] = this.bairro;
+        fields[RUA_FIELD.fieldApiName] = this.rua;
+        fields[NUMERO_FIELD.fieldApiName] = this.numero;
+        fields[COMPLEMENTO_FIELD.fieldApiName] = this.complemento;
+
+        const recordInput = {
+            apiName: PACIENTE_OBJECT.objectApiName,
+            fields
+        };
+
+        try {
+            const result = await createRecord(recordInput);
+
+            setTimeout(() => {
+                this[NavigationMixin.Navigate]({
+                    type: 'standard__recordPage',
+                    attributes: {
+                        recordId: result.id,
+                        objectApiName: PACIENTE_OBJECT.objectApiName,
+                        actionName: 'view'
+                    }
+                });
+                this.showSpinner = false;
+            }, 1000);
+
+             setTimeout(() => {
+                 this.showToast('Sucesso', 'Paciente cadastrado com sucesso!', 'success', 'dismissable');
+            }, 1300);
+        } catch (error) {
+            console.error('Erro ao criar paciente:', error);
+            alert('Erro ao cadastrar paciente.');
+        }
+    }
+
+    showToast(title, message, variant, mode) {
+        this.dispatchEvent(new ShowToastEvent({
+            title: title,
+            message: message,
+            variant: variant,
+            mode: mode
+        }));
+    }
+}
---
+++ force-app/main/default/lwc/minhasConsultas/minhasConsultas.js
@@ -0,0 +1,61 @@
+import { LightningElement, wire, track } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
+import USER_ID from '@salesforce/user/Id'; // <-- ID do usuário logado
+import NAME_FIELD from '@salesforce/schema/User.Name';
+
+import getConsultasByMedico from '@salesforce/apex/MinhasConsultasController.getConsultasByMedico';
+
+import { NavigationMixin } from 'lightning/navigation';
+
+const COLUMNS = [
+    {
+        label: 'Consulta',
+        fieldName: 'consultaLink',
+        type: 'url',
+        typeAttributes: { label: { fieldName: 'Name' }, target: '_blank' }
+    },
+    { label: 'Paciente', fieldName: 'Paciente' },
+    { label: 'Hospital', fieldName: 'Hospital__c' },
+    { label: 'Data', fieldName: 'Data__c', type: 'date' },
+];
+
+export default class MinhasConsultas extends NavigationMixin(LightningElement) {
+    @track consultas = [];
+    @track possuiConsulta = false;
+    @track showSpinner = false;
+    @track nomeUsuario = '';
+    columns = COLUMNS;
+
+    @wire(getRecord, { recordId: USER_ID, fields: [NAME_FIELD] })
+    userRecord({ error, data }) {
+        if (data) {
+            this.nomeUsuario = data.fields.Name.value;
+        } else if (error) {
+            console.error('Erro ao recuperar nome do usuário:', error);
+        }
+    }
+
+    connectedCallback() {
+        this.getConsultas();
+    }
+
+    async getConsultas() {
+        this.showSpinner = true;
+        try {
+            const result = await getConsultasByMedico();
+            if (result && result.length > 0) {
+                this.consultas = result.map(row => ({
+                    ...row,
+                    Paciente: row.Paciente__r?.Name,
+                    consultaLink: '/' + row.Id
+                }));
+                this.possuiConsulta = true;
+            }
+        } catch (error) {
+            console.error('Erro ao recuperar as consultas:', JSON.stringify(error));
+            this.possuiConsulta = false;
+        } finally {
+            this.showSpinner = false;
+        }
+    }
+}
